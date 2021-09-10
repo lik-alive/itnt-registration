d0e45c98e37cc22019669c9097166bafd07700c7
lik-alive <kirshdv@gmail.com>
2021-09-10 21:05:12 +0400

    r

--- backup.sh
+++ backup.sh
@@ -1,14 +1,18 @@
-echo 'itnt-registration backup started'
-ROOT=$1/itnt-registration
+title=itnt-registration
+
+echo $title backup started
+ROOT=$1/$title
 mkdir $ROOT
 
 # Backup web files
 WEBPATH=$ROOT/web
 mkdir $WEBPATH
-rsync -a --info=progress2 . $WEBPATH --exclude .git
+#rsync -a --info=progress2 . $WEBPATH --exclude .git
+tar cf - --exclude ".git" . | pv -s $(du -sb . | awk '{print $1}') | gzip > $WEBPATH/$title.tar.gz
 
 # Backup db
 DBPATH=$ROOT/db
 mkdir $DBPATH
 mysqldump -u$2 -p$3 --databases itntregdb > $DBPATH/dump.sql 2> /dev/null
-echo 'itnt-registration backup finished'
+
+echo $title backup finished
