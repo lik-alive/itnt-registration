058e5e2c789fefd4c10580ee8cb94cf6de717974
lik-alive <kirshdv@gmail.com>
2026-09-15 02:59:15 +0400

    security  improvements

--- init.php
+++ init.php
@@ -7,9 +7,11 @@ ob_start();
 require __DIR__ . '/wp-blog-header.php';
 ob_get_clean();
 
-$user_id = wp_create_user($env['DB_USER'], $env['DB_PASS']);
-update_user_meta($user_id, 'zi_capabilities', ['administrator' => true]);
-update_user_meta($user_id, 'first_name', 'Администратор');
+if (!username_exists($env['DB_USER'])) {
+    $user_id = wp_create_user($env['DB_USER'], $env['DB_PASS']);
+    update_user_meta($user_id, 'zi_capabilities', ['administrator' => true]);
+    update_user_meta($user_id, 'first_name', 'Администратор');
+}
 
 echo "Success\n";
 exit();
